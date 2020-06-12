05595672f3838aa6e14970c959701b64c63b7237
Lucas <lccaputo@hotmail.com>
2020-06-12 17:54:33 -0300

    feat: adiciona arquivo com os dados dos cards

---
+++ Desafio-3.0-Iniciando-o-Back-end/data.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id: "6wn8hpUcEcM",
+        title:
+            "DRAG AND DROP RAIZ COM A API DO HTML, CSS E JS, JDHBUFUSDB USDBCUSDC USDBCUSD CISCBIDS",
+        duration: "55 min",
+        price: " Free"
+    },
+    {
+        id: "JzQuJSJoPeM",
+        title: "GERADOR DE ENERGIA SEM FIM - ENERGIA LIVRE PARA SEMPRE",
+        duration: "8 min",
+        price: " Free"
+    },
+    {
+        id: "EhnXaybirdA",
+        title:
+            "CRIANDO UM PROJETO DO ZERO PARA SEU PORTFÓLIO, HSACBHSABC ASUBAUSC ABSUBA AUSBXAUSXB DSDC SDCSA DCSADC SADC SDC SDC",
+        duration: "45 min",
+        price: " Free"
+    }
+];

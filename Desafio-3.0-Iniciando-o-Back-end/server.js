bcbe563733e23b78ca71349e5c3e0927f558508c
Lucas <lccaputo@hotmail.com>
2020-06-12 14:35:15 -0300

    refactor: refatoração do server mudando o arquivo da view para .njk

--- Desafio-3.0-Iniciando-o-Back-end/server.js
+++ Desafio-3.0-Iniciando-o-Back-end/server.js
@@ -3,9 +3,9 @@ const nunjucks = require("nunjucks");
 
 const server = express();
 
-server.use(express.static('public'));
+server.use(express.static("public"));
 
-server.set("view engine", "html");
+server.set("view engine", "njk");
 
 nunjucks.configure("views", {
     express: server

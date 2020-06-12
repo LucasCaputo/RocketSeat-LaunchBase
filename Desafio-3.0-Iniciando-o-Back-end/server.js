0abe7b55d5254c1125f1c75e253f5a45b6c319a2
Lucas <lccaputo@hotmail.com>
2020-06-12 17:51:42 -0300

    feat: adiciona dados para serem usados no html

--- Desafio-3.0-Iniciando-o-Back-end/server.js
+++ Desafio-3.0-Iniciando-o-Back-end/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const videos = require("./data");
 
 const server = express();
 
@@ -8,15 +9,34 @@ server.use(express.static("public"));
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 });
 
 server.get("/", function (req, res) {
-    return res.render("about");
+    const data = {
+        avatarUrl:
+            "https://avatars2.githubusercontent.com/u/51523433?s=460&u=b484a77c22f9ff70c975e242ae41fabdce39c780&v=4",
+        name: "Lucas Reis Caputo",
+        role: "Desenvolvedor",
+        description: "Focado em transformar ideias em produtos",
+        links: [
+            {
+                name: "GitHub",
+                url: "https://github.com/LucasCaputo"
+            },
+            {
+                name: "LinkedIn",
+                url: "https://www.linkedin.com/in/lucascaputo/"
+            }
+        ]
+    };
+
+    return res.render("about", { about: data });
 });
 
 server.get("/courses", function (req, res) {
-    return res.render("courses");
+    return res.render("courses", { items: videos });
 });
 
 server.use(function (req, res) {

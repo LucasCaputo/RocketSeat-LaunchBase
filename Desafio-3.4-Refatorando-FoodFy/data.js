fb9d33cec80841b0cdbd947335424a732d52a90a
Lucas <lccaputo@hotmail.com>
2020-06-14 21:58:51 -0300

    refactor: refatoração do FoodFy utilizando servidor node e nunjuncks

---
+++ Desafio-3.4-Refatorando-FoodFy/data.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: "6wn8hpUcEcM",
+        image: "/assets/frango-assado.png",
+        title: "FRANGO ASSADO AO MOLHO AGRIDOCE DE PIMENTA",
+        description:
+            "O frango inteiro deve ser limpo, temperado e levado ao forno, após assado adicionar o molho agridoce de pimenta",
+        duration: "55 min",
+        price: "R$35 - R$50",
+        by: "Lucas Caputo"
+    },
+    {
+        id: "JzQuJSJoPeM",
+        image: "/assets/hamburguer.png",
+        title: "HAMBURGUER ARTESANAL COM MOLHO CHEDDAR",
+        description:
+            "Preparar com o hamburguer de carne bovina e fritar, acrescentar bacon e cheddar e pão com gergelin",
+        duration: "30 min",
+        price: "R$15 - R$30",
+        by: "JP hops"
+    },
+    {
+        id: "JzQuJSJoPeM",
+        image: "/assets//macarrao.png",
+        title: "MACARRÃO AO FORNO COM COGUMELOS",
+        description:
+            "Cozinhar o macarrão, fazer o molho de cogumelos, colocar o macarrão e o molho em uma assadeira e cobrir com mussarela",
+        duration: "80 min",
+        price: "R$40 - R$60",
+        by: "Chapão da Massa"
+    }
+];
